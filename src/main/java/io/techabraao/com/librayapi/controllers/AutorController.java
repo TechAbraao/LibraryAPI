71cd915fac618691957b2702071ddb0a8b1e558b
Abraão Santos <abraao.santos@sec4you.com.br>
2025-07-04 00:15:22 -0300

    estudos

--- src/main/java/io/techabraao/com/librayapi/controllers/AutorController.java
+++ src/main/java/io/techabraao/com/librayapi/controllers/AutorController.java
@@ -3,17 +3,18 @@ package io.techabraao.com.librayapi.controllers;
 import io.techabraao.com.librayapi.dto.AutorDTO;
 import io.techabraao.com.librayapi.models.Autor;
 import io.techabraao.com.librayapi.services.AutorService;
-import org.apache.coyote.Response;
-import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/autores")
@@ -45,11 +46,27 @@ public class AutorController {
                 .body(Map.of("message", autor))
                 .created(location)
                 .build();
+    };
+
+    @GetMapping
+    public ResponseEntity<Map<String, List<AutorDTO>>> pesquisar(
+            @RequestParam(value = "nome", required = false) String nome,
+            @RequestParam(value = "nacionalidade", required = false) String nacionalidade
+    ) {
+
+        List<Autor> lista = autorService.pesquisa(nome, nacionalidade);
+        List<AutorDTO> resultado = lista.stream().map(
+                autor -> new AutorDTO(
+                        autor.getNome(),
+                        autor.getDataNascimento(),
+                        autor.getNacionalidade()
+                )
+        ).collect(Collectors.toList());
+
+        return ResponseEntity
+                .status(HttpStatus.FOUND)
+                .body(Map.of("message", resultado));
     }
-
-    ;
-
-
 
     @GetMapping("/{id}")
     public ResponseEntity<AutorDTO> obterDetalhesPeloId(@PathVariable("id") String id) {
@@ -88,4 +105,31 @@ public class AutorController {
                 .body(Map.of("message", "Deletado com sucesso."));
 
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Map<String, String>> atualizar(@PathVariable("id") String id, @RequestBody AutorDTO dto) {
+        UUID identifier = UUID.fromString(id);
+
+        Optional<Autor> buscandoAutor = autorService.obterPorId(identifier);
+
+        if (buscandoAutor.isEmpty()) {
+            return ResponseEntity
+                    .notFound()
+                    .build();
+        }
+
+        Autor encontrouAutor = buscandoAutor.get();
+        encontrouAutor.setNome(dto.nome());
+        encontrouAutor.setDataNascimento(dto.dataNascimento());
+        encontrouAutor.setNacionalidade(dto.nacionalidade());
+        autorService.atualizar(encontrouAutor);
+
+        return ResponseEntity
+                .ok()
+                .body(Map
+                        .of("mensagem", "atualizado com sucesso."));
+    }
+
+    ;
+
 }
--- src/main/java/io/techabraao/com/librayapi/services/AutorService.java
+++ src/main/java/io/techabraao/com/librayapi/services/AutorService.java
@@ -4,6 +4,7 @@ import io.techabraao.com.librayapi.models.Autor;
 import io.techabraao.com.librayapi.repository.AutorRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -25,6 +26,14 @@ public class AutorService {
         return Boolean.TRUE;
     }
 
+    // Atualizar
+    public void atualizar(Autor autor) {
+        if (autor.getId() == null) {
+            throw new IllegalArgumentException("É necessário que o autor já esteja no bd.");
+        };
+        autorRepository.save(autor);
+    }
+
     // Deletar no BD
     public void deletarPorId(UUID id) {
        autorRepository.deleteById(id);
@@ -35,4 +44,19 @@ public class AutorService {
         return autorRepository.findById(id);
     }
 
+    // Buscar nome e nacionalidade
+    public List<Autor> pesquisa(String nome, String nacionalidade) {
+        if (nome != null && nacionalidade != null) {
+            return autorRepository.findByNomeAndNacionalidade(nome, nacionalidade);
+        }
+        if (nome != null) {
+            return autorRepository.findByNome(nome);
+        }
+
+        if (nacionalidade != null) {
+            return autorRepository.findByNacionalidade(nacionalidade);
+        }
+
+        return autorRepository.findAll();
+    }
 }
